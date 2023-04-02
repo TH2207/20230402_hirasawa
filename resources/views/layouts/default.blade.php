378d74253063ca709c95a8d1e1e5a9ca5cc237b2
TH2207 <member_all_out@yahoo.co.jp>
2023-04-02 11:54:18 +0900

    タイトル名を更新

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>COACHTECH</title>
+  <title>Rese</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script src="{{ asset('js/app.js') }}" defer></script>
   @yield('css')
